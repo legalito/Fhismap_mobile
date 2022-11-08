ea24fdfe1e4adde65028dc732916a4dd09b9377e
legalito <hlegal44@hotmail.fr>
2022-11-08 16:33:13 +0100

    Initial commit

---
+++ web/lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl:"https://fishmap.fr" ,
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
